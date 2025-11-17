e1d105efbac1b1e46c607ce579b301fe38946588
hanine <haninebenamor45@gmail.com>
2025-11-17 18:09:48 +0100

    the logic .ts

--- TTC-app/src/app/ttc/ttc.component.ts
+++ TTC-app/src/app/ttc/ttc.component.ts
@@ -2,13 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ttc',
-  standalone: true,
-  imports: [],
   templateUrl: './ttc.component.html',
-  styleUrl: './ttc.component.css'
+  styleUrls: ['./ttc.component.css']
 })
 export class TtcComponent {
+  price: number = 0;      // default price
+  quantity: number = 1;   // default quantity
+  tva: number = 18;       // default TVA %
 
+  calculateTTC(): number {
+    let discount = 0;
+
+    if (this.quantity >= 10 && this.quantity <= 15) discount = 0.2;
+    else if (this.quantity > 15) discount = 0.3;
+
+    const totalHT = this.price * this.quantity * (1 - discount);
+    return totalHT * (1 + this.tva / 100);
+  }
 }
-
-
