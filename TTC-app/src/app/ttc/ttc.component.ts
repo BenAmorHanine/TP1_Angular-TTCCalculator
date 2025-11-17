ea3e19f2613587b2b5172e52fb4aef87dc855b1d
hanine <haninebenamor45@gmail.com>
2025-11-17 19:14:02 +0100

    app done

--- TTC-app/src/app/ttc/ttc.component.ts
+++ TTC-app/src/app/ttc/ttc.component.ts
@@ -1,8 +1,61 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { DecimalPipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-ttc',
+  standalone: true,
+  imports: [FormsModule, DecimalPipe, CommonModule],
+  templateUrl: './ttc.component.html',
+  styleUrls: ['./ttc.component.css']
+})
+export class TtcComponent {
+  prixHT: number = 0;
+  quantite: number = 1;
+  tva: number = 18;
+
+  get remisePourcentage(): number {
+    if (this.quantite >= 10 && this.quantite <= 15) return 0.2;
+    if (this.quantite > 15) return 0.3;
+    return 0;
+  }
+
+   get prixUnitaireTTC(): number {
+    return this.prixHT + (this.prixHT * this.tva / 100);
+  }
 
 
+
+  get remiseMontant(): number {
+    return this.prixUnitaireTTC * this.quantite * this.remisePourcentage;
+  }
+
+  // Prix total TTC après remise
+  get prixTotalTTC(): number {
+    return (this.prixUnitaireTTC * this.quantite) - this.remiseMontant;
+  }
+
+  // Prix unitaire TTC après remise
+  //not needed not used
+  get prixUnitaireTTC_apresRemise(): number {
+    return this.prixUnitaireTTC * (1 - this.remisePourcentage);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/***
 @Component({
   selector: 'app-ttc',
   standalone: true,
@@ -25,3 +78,4 @@ export class TtcComponent {
     return totalHT * (1 + this.tva / 100);
   }
 }
+*/
